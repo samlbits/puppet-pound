f656ac78e258b4bbd579a47dab22fae134013e15
Fredrik Thulin <fredrik@thulin.net>
2013-12-01 16:19:09 +0100

    fix server_cert

--- manifests/entry.pp
+++ manifests/entry.pp
@@ -4,9 +4,9 @@ define pound::entry ($listen_ip,
     $listen_protocol,
     $head_require,
     $backend_ips,
-    $backend_port) {
-
-
+    $backend_port,
+    $server_cert = '/etc/ssl/private/ssl-cert-snakeoil.key',
+) {
     concat::fragment {
         "pound_entry-${name}" :
             target => '/etc/pound/pound.cfg',
